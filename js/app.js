846dec650616557a5d25914cee962422d23f98b7
vv <vivyanw@uw.edu>
2014-11-04 10:15:28 -0800

    added angular to the demo!

--- js/app.js
+++ js/app.js
@@ -5,3 +5,25 @@
 
 "use strict";
 
+angular.module('MoviesApp', [])
+	.controller('MoviesController', function($scope) {
+		// initialize variables with list of movies such that it'll be accessed in the view
+		$scope.movies = movies;
+
+		//set up our sort column in scope
+		$scope.sortCol = 'rank';
+		$scope.sortReverse = false;
+
+		$scope.sortBy = function (colName) {
+			if($scope.sortCol == colName){
+				$scope.sortReverse = !$scope.sortReverse;
+			} else {
+				$scope.sortReverse = false;
+				$scope.sortCol =  colName;
+			}
+		};
+
+		$scope.isSortedBy = function (colName) {
+			return $scope.sortCol == colName;
+		}
+	});
